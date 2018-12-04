b60236003652f241020b466733ac1b559a5bb24a
wescottsharples <wesc@ttsharpl.es>
2018-12-04 07:39:23 +0000

    Fix betty style

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function checks a linked list for a cycle
+ * @list: pointer to first node in linked list
+ * Return: 1 if cyclical, else 0.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *last, *new;
+
+	if (!list)
+		return (0);
+	last = list;
+	new = last->next;
+
+	while (1)
+	{
+		if (new == last)
+			return (1);
+		new = new->next;
+		if (!new)
+			return (0);
+		new = new->next;
+		if (!new)
+			return (0);
+		last = last->next;
+	}
+
+	return (0);
+}
