f12b569bee2fb6ec071402b6248672e6cdb581c2
wescottsharples <wesc@ttsharpl.es>
2018-12-05 05:40:12 +0000

    Fix 'or' operator to 'and'

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if chr(c) != 'q' or chr(c) != 'e':
+    if chr(c) != 'q' and chr(c) != 'e':
         print("{}".format(chr(c)), end='')
