1e69a0746af6c2e476b4b2b6f19d3529fdabb8c2
wescottsharples <wesc@ttsharpl.es>
2018-12-12 04:21:14 +0000

    Add perenthesis around conditionals

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if my_list is None or idx is None:
+    if (my_list is None) or (idx is None):
         return
-    if idx < 0:
+    if (idx < 0):
         return my_list
-    elif idx > len(my_list):
+    elif (idx > len(my_list)):
         return my_list
     else:
         my_list[idx] = element
