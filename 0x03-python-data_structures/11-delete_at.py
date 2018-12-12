6d36e2ca8fd4c94e3a67d87545397dd06b9d5578
wescottsharples <wesc@ttsharpl.es>
2018-12-12 07:38:58 +0000

    Add check of idx and len

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,8 +2,8 @@
 def delete_at(my_list=[], idx=0):
     if (my_list is None) or (idx is None):
         return my_list
-    if (idx < 0):
-        my_list
+    if (idx < 0) or (idx >= len(my_list)):
+        return my_list
 
     del my_list[idx]
     return (my_list)
