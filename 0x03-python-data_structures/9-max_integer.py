4964e76d9e7605c895458538f4961a62ec73bf7f
wescottsharples <wesc@ttsharpl.es>
2018-12-12 07:26:12 +0000

    Add check for list len

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if (my_list is None) or (len(my_list) < 1):
         return
 
     biggest = my_list[0]
     for i in my_list:
-        if biggest < i:
+        if (biggest < i):
             biggest = i
     return biggest
