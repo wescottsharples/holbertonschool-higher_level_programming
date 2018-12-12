4f72b4091b750abed5b442512ed5efd13bc55214
wescottsharples <wesc@ttsharpl.es>
2018-12-12 04:09:00 +0000

    Add equal to in len comparison

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
     if (my_list is None) or (idx is None):
         return
     if (idx < 0):
         return
-    elif (idx > len(my_list)):
+    elif (idx >= len(my_list)):
         return
     else:
         return my_list[idx]
