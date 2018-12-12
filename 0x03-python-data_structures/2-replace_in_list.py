a12c16228d94bc40d2f5305c43606b280d22d05a
wescottsharples <wesc@ttsharpl.es>
2018-12-12 04:22:30 +0000

    Add equal to condition for len check

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
         return
     if (idx < 0):
         return my_list
-    elif (idx > len(my_list)):
+    elif (idx >= len(my_list)):
         return my_list
     else:
         my_list[idx] = element
