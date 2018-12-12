25aeaa1c2ad9b1a41572d9e9cb3b1ecdd96ee9ef
wescottsharples <wesc@ttsharpl.es>
2018-12-12 04:07:16 +0000

    Add perenthesis around if statements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    if my_list is None or idx is None:
+    if (my_list is None) or (idx is None):
         return
-    if idx < 0:
+    if (idx < 0):
         return
-    elif idx > len(my_list):
+    elif (idx > len(my_list)):
         return
     else:
         return my_list[idx]
