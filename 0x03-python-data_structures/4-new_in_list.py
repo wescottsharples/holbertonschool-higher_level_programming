0654105943f5cc5c79ec9c10f97ade9fc7dbcd35
wescottsharples <wesc@ttsharpl.es>
2018-12-12 04:36:46 +0000

    Fix copying mechanism

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpy_list = my_list
+    cpy_list = my_list.copy()
     if (my_list is None) or (idx is None) or (element is None):
         return cpy_list
     if (idx < 0) or (idx >= len(my_list)):
