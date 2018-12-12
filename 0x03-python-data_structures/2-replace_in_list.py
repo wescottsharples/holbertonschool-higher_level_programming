efb388c48372ffb3a039ab4c67b2511672989286
wescottsharples <wesc@ttsharpl.es>
2018-12-12 04:16:55 +0000

    Add argument check and deleted extra line

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
 def replace_in_list(my_list, idx, element):
+    if my_list is None or idx is None:
+        return
     if idx < 0:
         return my_list
     elif idx > len(my_list):
