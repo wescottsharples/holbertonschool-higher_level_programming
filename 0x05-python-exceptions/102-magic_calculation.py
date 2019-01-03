e76b778620fb4e6e59e5946723d711ed762db933
wescottsharples <wesc@ttsharpl.es>
2019-01-03 07:29:48 +0000

    Fix return value

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += a ** b / i
-                return result
         except:
             result = a + b
             return result
+    return result
