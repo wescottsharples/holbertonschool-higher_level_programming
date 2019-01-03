b5d1f02d7460a9e1b49df893f7d7667abec988c7
wescottsharples <wesc@ttsharpl.es>
2019-01-03 05:52:13 +0000

    Function prints ints from list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        print("{:d}".format())
+        count++;
+    except:
+        continue
+    print()
+    return count
