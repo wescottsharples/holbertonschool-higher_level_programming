18720b00d90a332b72b48ee105fb8073ffc6439f
wescottsharples <wesc@ttsharpl.es>
2019-01-03 06:09:40 +0000

    Fix syntax errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
-        print("{:d}".format())
-        count++
-    except (TypeError, ValueError):
-        continue
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            continue
     print()
     return count
