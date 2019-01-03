4b2d712357fafe056ff54444ae046ae9b0d5dcac
wescottsharples <wesc@ttsharpl.es>
2019-01-03 05:35:31 +0000

    Fix syntax error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print("{}.format(my_list[i]), end=""")
+            print("{}".format(my_list[i]), end="")
     except IndexError:
         print()
         return i
+    print()
+    return i
