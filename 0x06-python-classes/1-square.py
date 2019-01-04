ffdd2978859c4bdcbebe6967982681f27216badb
wescottsharples <wesc@ttsharpl.es>
2019-01-04 00:37:16 +0000

    Script defines class with private instance attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class Square that defines a square"""
+
+
+class Square:
+    """Contains square size"""
+    def __init__(self, size):
+        self.__size = size
+    pass
