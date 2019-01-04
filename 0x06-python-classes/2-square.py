776fb9fc59f18f5cb713bf27605ad6595b520431
wescottsharples <wesc@ttsharpl.es>
2019-01-04 00:53:02 +0000

    Fix TypeError message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """Contains square size"""
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         else:
