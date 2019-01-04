8edb7887c468421eb56ce4b45f30f4c264fbcbad
wescottsharples <wesc@ttsharpl.es>
2019-01-04 01:09:34 +0000

    Fix use of __init__

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,4 +21,9 @@ class Square:
 
     @size.setter
     def size(self, value):
-        __init__(self, value)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
