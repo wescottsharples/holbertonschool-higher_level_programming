fd3c0a261f519d659635be76477501291dd20771
wescottsharples <wesc@ttsharpl.es>
2019-01-04 01:14:22 +0000

    Swap use of size for value parameter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,9 +21,9 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
