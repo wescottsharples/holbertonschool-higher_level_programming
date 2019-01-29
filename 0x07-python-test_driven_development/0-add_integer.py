dcc96bcc234aea46fb6d385c333c3b6c8d84ba6c
wescottsharples <wesc@ttsharpl.es>
2019-01-29 22:28:43 +0000

    Function returns sum of two ints

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Contains function that returns the sum of two ints"""
+""" Contains function that returns addition of two ints """
 
 
 def add_integer(a, b=98):
-    """Returns sum of two ints, second parameter defaults to 98"""
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    """ Returns sum of two integers, second parameter defaults to 98 """
+    if not isinstance(a, int):
+        if not isinstance(a, float):
+            raise TypeError("a must be an integer")
+        a = int(a)
+    if not isinstance(b, int):
+        if not isinstance(b, float):
+            raise TypeError("b must be an integer")
+        b = int(b)
+    return a + b
