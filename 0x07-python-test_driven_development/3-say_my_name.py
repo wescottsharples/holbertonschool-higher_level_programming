b3825d4e2933a40f8a776bc0f9a3af6e21db1ac3
wescottsharples <wesc@ttsharpl.es>
2019-01-30 02:27:49 +0000

    Function prints first and last name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Contains function that prints two strings"""
+""" Contains function which prints out first and last name """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints first and last name as strings"""
+    """ Prints "My name is " followed by first and last name """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is", first_name, last_name)
+    print("My name is {:s} {:s}".format(first_name, last_name))
