acb4fed570858ad516d47391d67e0f5e9da5b145
wescottsharples <wesc@ttsharpl.es>
2019-01-30 02:44:05 +0000

    Function prints a square made of the '#' symbol

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Contains function that prints a square"""
+""" Contains function which prints a square """
 
 
 def print_square(size):
-    """Prints a square made of # symbol"""
+    """ Prints a square using '#' character """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    row = '#' * size
-    for i in range(size):
-        print(row)
+    for _ in range(size):
+        print('#' * size)
