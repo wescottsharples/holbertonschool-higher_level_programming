e4a2de4cefb66d31216508c0de1e8e91f2eea1ea
wescottsharples <wesc@ttsharpl.es>
2019-01-30 01:27:41 +0000

    Fix Pep8 style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,9 +3,11 @@
 
 
 def matrix_divided(matrix, div):
+    type_warning = "matrix must be a matrix (list of lists) of integers/floats"
+
     """ Divides all elements of a matrix """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(type_warning)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
@@ -13,17 +15,17 @@ def matrix_divided(matrix, div):
 
     row_len = len(matrix[0])
     if row_len == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(type_warning)
     new_matrix = []
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(type_warning)
         if not len(row) == row_len:
             raise TypeError("Each row of the matrix must have the same size")
         new_row = []
         for i in row:
             if not isinstance(i, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(type_warning)
             new_row.append(round(i / div, 2))
         new_matrix.append(new_row)
     return new_matrix
