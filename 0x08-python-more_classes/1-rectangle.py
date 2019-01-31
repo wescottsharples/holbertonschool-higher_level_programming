304e993959460713322809cd2e12d93db1252e31
wescottsharples <wesc@ttsharpl.es>
2019-01-31 02:19:51 +0000

    Defines Rectangle class with width and height attrs

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module contains class Rectangle """
+
+
+class Rectangle:
+    """ Defines Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """ Inits class """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+       """ Retrieves width attribute """
+       return self.__width
+
+    @property
+    def height(self):
+       """ Retrieves height attribute """
+       return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ Sets width attribute """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ Sets width attribute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
