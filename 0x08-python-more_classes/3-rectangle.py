657ed48821de7976b96d7fd3d35010fe14a448fe
wescottsharples <wesc@ttsharpl.es>
2019-01-31 02:46:39 +0000

    Expands on Rectangle class with __str__ method

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Module contains class Rectangle """
+
+
+class Rectangle:
+    """ Defines Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """ Inits class """
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """ Prints Rectangle made of #'s """
+        return '\n'.join(('#' * self.__width) for _ in range(self.__height))
+
+    @property
+    def width(self):
+        """ Retrieves width attribute """
+        return self.__width
+
+    @property
+    def height(self):
+        """ Retrieves height attribute """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ Sets width attribute """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ Sets width attribute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Returns area """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Returns perimeter """
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
