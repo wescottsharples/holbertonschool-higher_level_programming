4c7ad0875644b7591e589e8c95fe8a01e1f0a3fc
wescottsharples <wesc@ttsharpl.es>
2019-03-06 04:04:30 +0000

    Lists all genres in database and # of shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all genres in database and displays number of shows in each
+SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_shows DESC;
