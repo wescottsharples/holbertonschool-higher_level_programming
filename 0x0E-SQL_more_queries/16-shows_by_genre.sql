898e67f19d4cb2b9a30035066bc0dfdef5dc1874
wescottsharples <wesc@ttsharpl.es>
2019-03-06 05:47:35 +0000

    Fix use of left outer

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows with genres in database
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_genres.name;
