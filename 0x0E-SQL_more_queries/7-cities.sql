2e33316be1aa75adb83d75a3329da7ded06255f9
wescottsharples <wesc@ttsharpl.es>
2019-03-06 01:11:39 +0000

    Creates a database and table with foreign keys

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates the database 'hbtn_0a_usa' and table 'citites'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
