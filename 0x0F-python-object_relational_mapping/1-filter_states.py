e41cb322f6212d8823e518a94db864b7f23c69b2
wescottsharples <wesc@ttsharpl.es>
2019-03-29 19:01:16 +0000

    Script lists all states starting with 'N' in 'hbtn_0e_0_usa'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all states starting with 'N' from provided database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database and create cursor
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    # select and list states in database
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    # cleanup
+    cur.close()
+    db.close()
