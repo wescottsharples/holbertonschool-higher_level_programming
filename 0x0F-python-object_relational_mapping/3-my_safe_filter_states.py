aa8e3a2f0e635b36730db0dc8af3fabff24f9c45
wescottsharples <wesc@ttsharpl.es>
2019-03-29 20:30:48 +0000

    Script safely selects specified state from database

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+displays all states that match argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database and create cursor
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    # select and list states in database
+    cur.execute("""
+                SELECT *
+                FROM states
+                WHERE name=%s
+                ORDER BY id ASC
+                """, (argv[4],))
+    states = cur.fetchall()
+    for state in states:
+        if (state[1] == argv[4]):
+            print(state)
+
+    # cleanup
+    cur.close()
+    db.close()
