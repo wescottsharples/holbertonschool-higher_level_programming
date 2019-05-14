501d5d36e3e5a9748a26051804d1756b62cf960f
wescottsharples <wesc@ttsharpl.es>
2019-05-14 23:28:22 +0000

    Fix function proto

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-fs.readFile(process.argv[2], function (err, r) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('code: ' + r.statusCode);
-    }
+request(process.argv[2], function (err, r) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + r.statusCode);
+  }
 });
