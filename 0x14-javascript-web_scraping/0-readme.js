b2a4e8da15218378b673f21300bc5ea7d32a5697
wescottsharples <wesc@ttsharpl.es>
2019-05-14 23:28:49 +0000

    Fix semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
