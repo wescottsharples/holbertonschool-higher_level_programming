5c0688d7c074f1e69ff25d22378f2222ea80ed89
wescottsharples <wesc@ttsharpl.es>
2019-05-15 00:22:38 +0000

    Fix url parameter

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/movies' + process.argv[2];
-request.get(url, function (err, r, body) {
+request.get(process.argv[2], function (err, r, body) {
   if (err) {
     console.log(err);
-  } else if r.statusCode !== 200 {
-    console.log("Error code:" + r.statusCode)
   } else {
     let count = 0;
     let movies = JSON.parse(body).results;
