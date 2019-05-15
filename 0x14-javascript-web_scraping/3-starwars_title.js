90bb8f40814f568ce2cdb84e194cb001fd9be329
wescottsharples <wesc@ttsharpl.es>
2019-05-15 00:23:41 +0000

    Add slash to url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films' + process.argv[2];
+let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (err, r, body) {
   if (err) {
     console.log(err);
   } else {
-    let body = JSON.parse(body);
+    body = JSON.parse(body);
     console.log(body.title);
   }
 });
